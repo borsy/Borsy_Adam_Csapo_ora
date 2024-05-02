1e0f031dd928d5605928fe869875178066cd08e5
Bereg Szgep <bereg@bereg.hu>
2024-05-02 17:20:34 +0200

    ...

---
+++ ConsoleApp20240502/Epulet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20240502
+{
+    public class Epulet
+    {
+        private int rang;
+        private string nev;
+        private string varos_orszag;
+        private double magassag_m;
+        private int magassag_ft;
+        private int emeletek_szama;
+        private int epites_eve;
+        private string alapanyag;
+        private string felhasznalas;
+
+        public Epulet(int rang, string nev, string varos_orszag, int magassag_m, int magassag_ft, int emeletek_szama, int epites_eve, string alapanyag, string felhasznalas)
+        {
+            this.rang = rang;
+            this.nev = nev;
+            this.varos_orszag = varos_orszag;
+            this.magassag_m = magassag_m;
+            this.magassag_ft = magassag_ft;
+            this.emeletek_szama = emeletek_szama;
+            this.epites_eve = epites_eve;
+            this.alapanyag = alapanyag;
+            this.felhasznalas = felhasznalas;
+        }
+    }
+}
--- ConsoleApp20240502/Program.cs
+++ ConsoleApp20240502/Program.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp20240502
 {
     class Program
     {
+
+       static List<Epulet> epuletek = new List<Epulet>();
+
         static void Main(string[] args)
         {
+            beolvasas();
+
+            Console.WriteLine("Program vége!");
+            Console.ReadLine();
+        }
+
+        private static void beolvasas()
+        {
+            using (StreamReader olvaso = new StreamReader("100_tallest_javitott.csv"))
+            {
+                olvaso.ReadLine(); // --fejléc kihagyása, nem kell
+                while (!olvaso.EndOfStream)
+                {
+                    string[] sor = olvaso.ReadLine().Replace('"', ' ').Split(',');
+                    Epulet epulet = new Epulet(int.Parse(sor[0].Trim()), sor[1].Trim(), sor[2].Trim(), int.Parse(sor[3].Trim()), int.Parse(sor[4].Trim()), int.Parse(sor[5].Trim()), int.Parse(sor[6].Trim()), sor[7].Trim(), sor[8].Trim());
+                    epuletek.Add(epulet);
+                }
+            }
         }
     }
 }
