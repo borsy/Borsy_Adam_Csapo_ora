7b176010bfb20842829cc15ba4644d83ecf08e23
borsy <test@test.com>
2024-05-08 07:57:56 +0200

    ...

--- GUI20240502/Adatbazis_kapcsolat.cs
+++ GUI20240502/Adatbazis_kapcsolat.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace GUI20240502
+
+    Adatbazis_kapcsolat kapcsolat = new Adatbazis_kapcsolat();
 {
     internal class Adatbazis_kapcsolat
     {
